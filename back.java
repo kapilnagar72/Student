a5f6d326f445e73023f21feec61b25ce1da9b716
kapilnagar72 <kapilnagar14820@gmail.com>
2024-04-10 13:03:22 +0530

    new file add

---
+++ back.java
@@ -0,0 +1,19 @@
+
+package Student;
+
+import java.util.Scanner;
+
+/**
+ * back
+ */
+public class back {
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the user id num");
+        int num = sc.nextInt();
+        System.out.println("user id " + num);
+
+    }
+}
